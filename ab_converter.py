03a2b64e5c7f7c9eb8d551672328cb801bf38a8e
joelteply <joel@cambriantech.com>
2018-08-27 03:07:43 -0500

    skip categories

--- ab_converter.py
+++ ab_converter.py
@@ -16,6 +16,7 @@ from PIL import Image
 from scipy import misc
 import fnmatch
 import cv2
+import re
 
 #example usage:
 # CamVid dataset:
@@ -25,13 +26,18 @@ import cv2
 # --b_input_dir $datasets/CamVid/train_labels \
 # --output_dir $datasets/CamVidAB/train
 
-# ADE20K dataset:
+# ADE20K dataset (indoor only):
 # export datasets=../../datasets; \
 # python ab_converter.py \
 # --input_dir $datasets/ADE20K_2016_07_26/images/training \
+# --filter_categories $datasets/ADE20K_2016_07_26/indoor-categories.txt \
 # --a_match_exp "ADE_*.jpg" \
 # --b_match_exp "ADE_*_seg.png" \
-# --output_dir $datasets/ADE20KAB/train
+# --output_dir $datasets/ADE20K_indoor_AB/train
+
+# python pix2pix.py --mode train --output_dir ade20k_train --max_epochs 2000 --input_dir $datasets/ADE20KAB/train --which_direction AtoB
+
+# python pix2pix.py --mode train --output_dir ade20k_train --max_epochs 2000 --input_dir ADE20KAB/train --which_direction AtoB
 
 parser = argparse.ArgumentParser()
 
@@ -43,6 +49,8 @@ parser.add_argument("--b_input_dir", required=False, help="Target Input, image B
 parser.add_argument("--input_dir", required=False, help="Combined Source and Target Input Path")
 parser.add_argument("--a_match_exp", required=False, help="Source Input expression to match files")
 parser.add_argument("--b_match_exp", required=False, help="Source Input expression to match files")
+
+parser.add_argument("--filter_categories", required=False, help="Path to file with valid categories")
 
 # Place to output A/B images
 parser.add_argument("--output_dir", required=True, help="where to put output files")
@@ -56,15 +64,20 @@ def is_valid_image(path):
         return False
     return True
 
-def get_image_paths(path, expression=None):
+def get_image_paths(path, expression=None, filtered_dirs=None):
     file_names=[]
+
     #print("Checking %s for images" % path)
+    valid_image_dir = True
+
+    if not filtered_dirs is None and not os.path.basename(path) in filtered_dirs:
+    	valid_image_dir = False
 
     for file in os.listdir(path):
         file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
-        	file_names.extend(get_image_paths(file_path, expression))
-        elif is_valid_image(file_path) and (expression is None or fnmatch.fnmatch(file, expression)):
+        	file_names.extend(get_image_paths(file_path, expression, filtered_dirs))
+        elif valid_image_dir and is_valid_image(file_path) and (expression is None or fnmatch.fnmatch(file, expression)):
 			file_names.append(file_path)
 
     file_names.sort()
@@ -124,16 +137,57 @@ def hasParams(params):
 
 def main():
 
+	filtered_dirs = None
+	if not a.filter_categories is None:
+
+		if not os.path.isfile(a.filter_categories): 
+			print("Error: filter_categories file %s does not exist" % a.filter_categories)
+			return
+
+		filtered_dirs = []
+
+		with open(a.filter_categories) as f:
+			content = f.readlines()
+			content = [x.strip() for x in content] 
+
+		#/b/banquet_hall 38
+		for line in content:
+			category_search = re.search('/[a-z]/(\\w+)', line, re.IGNORECASE)
+			if category_search:
+				category = category_search.group(1)
+				filtered_dirs.append(category)
+
 	if not a.input_dir is None:
 		if not hasParams(["a_match_exp", "b_match_exp"]):
 			return
-		a_names=get_image_paths(a.input_dir, a.a_match_exp)
-		b_names=get_image_paths(a.input_dir, a.b_match_exp)
+
+		if not os.path.isdir(a.input_dir): 
+			print("Error: input_dir %s does not exist" % a.input_dir)
+			return
+
+		if not filtered_dirs is None:
+			filtered_dirs.append(os.path.basename(a.input_dir))
+
+		a_names=get_image_paths(a.input_dir, a.a_match_exp, filtered_dirs=filtered_dirs)
+		b_names=get_image_paths(a.input_dir, a.b_match_exp, filtered_dirs=filtered_dirs)
 	else:
 		if not hasParams(["a_input_dir", "b_input_dir"]):
 			return
-		a_names=get_image_paths(a.a_input_dir)
-		b_names=get_image_paths(a.b_input_dir)
+
+		if not os.path.isdir(a.a_input_dir): 
+			print("Error: a_input_dir %s does not exist" % a.a_input_dir)
+			return
+
+		if not os.path.isdir(a.b_input_dir): 
+			print("Error: b_input_dir %s does not exist" % a.b_input_dir)
+			return
+
+		if not filtered_dirs is None:
+			filtered_dirs.append(os.path.basename(a.a_input_dir))
+			filtered_dirs.append(os.path.basename(a.b_input_dir))
+
+		a_names=get_image_paths(a.a_input_dir, filtered_dirs=filtered_dirs)
+		b_names=get_image_paths(a.b_input_dir, filtered_dirs=filtered_dirs)
 
 	processFiles(a_names, b_names)	
 
