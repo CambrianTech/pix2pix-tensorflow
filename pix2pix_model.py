568cd79ed3fb796506753156e3285a48b73fd594
Robin Kahlow <robinkahlow@web.de>
2018-11-20 17:13:08 +0000

    initial estimator implementation using new shared module functionality

--- pix2pix_model.py
+++ pix2pix_model.py
@@ -1,19 +1,109 @@
-
 import tensorflow as tf
-import numpy as np
-import argparse
-import os
-import json
-import glob
-import random
-import collections
-import math
-import time
-import io
-from scipy import misc
-import cv2
+import cambrian
 
-Model = collections.namedtuple("Model", "outputs, predict_real, predict_fake, discrim_loss, discrim_grads_and_vars, gen_loss_GAN, gen_loss_L1, gen_grads_and_vars, train, gradient_penalty")
+EPS = 1e-12
+
+class Pix2PixModel(cambrian.nn.ModelBase):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.is_general_setup = False
+        self.is_train_setup = False
+
+    def _setup_general(self):
+        if self.is_general_setup:
+           raise Exception("General was already set up.")
+
+        with tf.variable_scope("generator"):
+            out_channels = sum([spec.channels for spec in self.args["b_specs"]])
+            self._outputs = create_generator(self.args, self.inputs, out_channels)
+
+    def _setup_train(self):
+        if self.is_train_setup:
+            raise Exception("Train was already set up.")
+
+         # create two copies of discriminator, one for real pairs and one for fake pairs
+        # they share the same underlying variables
+        with tf.name_scope("real_discriminator"):
+            with tf.variable_scope("discriminator"):
+                # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
+                predict_real = create_discriminator(self.args, self.inputs, self.targets)
+
+        with tf.name_scope("fake_discriminator"):
+            with tf.variable_scope("discriminator", reuse=True):
+                # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
+                predict_fake = create_discriminator(self.args, self.inputs, self.outputs)
+
+        with tf.name_scope("discriminator_loss"):
+            # minimizing -tf.log will try to get inputs to 1
+            # predict_real => 1
+            # predict_fake => 0
+            if self.args["gan_loss"] == "gan":
+                discrim_loss = tf.reduce_mean(-(tf.log(predict_real + EPS) + tf.log(1 - predict_fake + EPS)))
+            elif self.args["gan_loss"] == "wgan":
+                discrim_loss = tf.reduce_mean(predict_fake - predict_real)
+            else:
+                raise Exception("Unknown gan_loss:", self.args["gan_loss"])
+
+        with tf.name_scope("generator_loss"):
+            # predict_fake => 1
+            # abs(self.targets - self.outputs) => 0
+            if self.args["gan_loss"] == "gan":
+                gen_loss_GAN = tf.reduce_mean(-tf.log(predict_fake + EPS))
+            elif self.args["gan_loss"] == "wgan":
+                gen_loss_GAN = tf.reduce_mean(-predict_fake)
+            else:
+                raise Exception("Unknown gan_loss:", self.args["gan_loss"])
+
+            gen_loss_L1 = tf.reduce_mean(tf.abs(self.targets - self.outputs))
+            gen_loss = gen_loss_GAN * self.args["gan_weight"] + gen_loss_L1 * self.args["l1_weight"]
+
+         # Gradient penalty
+        if self.args["gp_weight"] is not None and self.args["gp_weight"] > 0:
+            with tf.name_scope("gradient_penalty"):
+                rand_interp = random_interpolate(self.targets, self.outputs)
+                with tf.variable_scope("discriminator", reuse=True):
+                    predict_rand_interp = create_discriminator(self.args, self.inputs, rand_interp)
+                gradient_penalty = self.args["gp_weight"] * get_gradient_penalty(self.args, predict_rand_interp, rand_interp)
+        else:
+            gradient_penalty = None
+
+        with tf.name_scope("discriminator_train"):
+            discrim_tvars = [var for var in tf.trainable_variables() if var.name.startswith("discriminator")]
+            discrim_optim = tf.train.AdamOptimizer(self.args["lr_d"], self.args["beta1"], self.args["beta2"])
+
+            discrim_total_loss = discrim_loss
+            if gradient_penalty is not None:
+                discrim_total_loss += gradient_penalty
+
+            discrim_grads_and_vars = discrim_optim.compute_gradients(discrim_total_loss, var_list=discrim_tvars)
+            discrim_train = discrim_optim.apply_gradients(discrim_grads_and_vars)
+
+        with tf.name_scope("generator_train"):
+            with tf.control_dependencies([discrim_train]):
+                gen_tvars = [var for var in tf.trainable_variables() if var.name.startswith("generator")]
+                gen_optim = tf.train.AdamOptimizer(self.args["lr_g"], self.args["beta1"], self.args["beta2"])
+                gen_grads_and_vars = gen_optim.compute_gradients(gen_loss, var_list=gen_tvars)
+                gen_train = gen_optim.apply_gradients(gen_grads_and_vars, global_step=tf.train.get_global_step())
+
+        # "gen_train" also includes discrim_train through control dependencies
+        self._train_op = gen_train
+        self._loss = gen_loss_L1
+        
+        self.metrics["gen_l1"] = gen_loss_L1
+        self.metrics["gen_gan"] = gen_loss_GAN
+        self.metrics["disc_gan"] = discrim_loss
+        self.metrics["disc_gp"] = gradient_penalty
+        self.metrics["gen_total"] = gen_loss
+        self.metrics["disc_total"] = discrim_total_loss
+
+    def set_inputs(self, inputs):
+        super().set_inputs(inputs)
+        self._setup_general()
+
+    def set_targets(self, targets):
+        super().set_targets(targets)
+        self._setup_train()
 
 def random_interpolate(a, b):
     shape = tf.concat((tf.shape(a)[0:1], tf.tile([1], [a.shape.ndims - 1])), axis=0)
@@ -28,131 +118,30 @@ def get_gradient_penalty(args, pred, x):
     gp = tf.reduce_mean((slopes - 1.)**2)
     return gp
 
-def create_model(args, inputs, targets, EPS):
-    
-    with tf.variable_scope("generator"):
-        out_channels = int(targets.get_shape()[-1])
-        outputs = create_generator(args, inputs, out_channels)
-
-    # create two copies of discriminator, one for real pairs and one for fake pairs
-    # they share the same underlying variables
-    with tf.name_scope("real_discriminator"):
-        with tf.variable_scope("discriminator"):
-            # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
-            predict_real = create_discriminator(args, inputs, targets)
-
-    with tf.name_scope("fake_discriminator"):
-        with tf.variable_scope("discriminator", reuse=True):
-            # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
-            predict_fake = create_discriminator(args, inputs, outputs)
-
-    with tf.name_scope("discriminator_loss"):
-        # minimizing -tf.log will try to get inputs to 1
-        # predict_real => 1
-        # predict_fake => 0
-        if args["gan_loss"] == "gan":
-            discrim_loss = tf.reduce_mean(-(tf.log(predict_real + EPS) + tf.log(1 - predict_fake + EPS)))
-        elif args["gan_loss"] == "wgan":
-            discrim_loss = tf.reduce_mean(predict_fake - predict_real)
-        else:
-            raise Exception("Unknown gan_loss:", args["gan_loss"])
-
-    with tf.name_scope("generator_loss"):
-        # predict_fake => 1
-        # abs(targets - outputs) => 0
-        if args["gan_loss"] == "gan":
-            gen_loss_GAN = tf.reduce_mean(-tf.log(predict_fake + EPS))
-        elif args["gan_loss"] == "wgan":
-            gen_loss_GAN = tf.reduce_mean(-predict_fake)
-        else:
-            raise Exception("Unknown gan_loss:", args["gan_loss"])
-
-        gen_loss_L1 = tf.reduce_mean(tf.abs(targets - outputs))
-        gen_loss = gen_loss_GAN * args["gan_weight"] + gen_loss_L1 * args["l1_weight"]
-
-    # Gradient penalty
-    if args["gp_weight"] is not None and args["gp_weight"] > 0:
-        with tf.name_scope("gradient_penalty"):
-            rand_interp = random_interpolate(targets, outputs)
-            with tf.variable_scope("discriminator", reuse=True):
-                predict_rand_interp = create_discriminator(args, inputs, rand_interp)
-            gradient_penalty = args["gp_weight"] * get_gradient_penalty(args, predict_rand_interp, rand_interp)
-    else:
-        gradient_penalty = None
-
-    with tf.name_scope("discriminator_train"):
-        discrim_tvars = [var for var in tf.trainable_variables() if var.name.startswith("discriminator")]
-        discrim_optim = tf.train.AdamOptimizer(args["lr_d"], args["beta1"], args["beta2"])
-
-        discrim_total_loss = discrim_loss
-        if gradient_penalty is not None:
-            discrim_total_loss += gradient_penalty
-
-        discrim_grads_and_vars = discrim_optim.compute_gradients(discrim_total_loss, var_list=discrim_tvars)
-        discrim_train = discrim_optim.apply_gradients(discrim_grads_and_vars)
-
-    with tf.name_scope("generator_train"):
-        with tf.control_dependencies([discrim_train]):
-            gen_tvars = [var for var in tf.trainable_variables() if var.name.startswith("generator")]
-            gen_optim = tf.train.AdamOptimizer(args["lr_g"], args["beta1"], args["beta2"])
-            gen_grads_and_vars = gen_optim.compute_gradients(gen_loss, var_list=gen_tvars)
-            gen_train = gen_optim.apply_gradients(gen_grads_and_vars)
-
-    global_step = tf.train.get_or_create_global_step()
-    incr_global_step = tf.assign(global_step, global_step+1)
-
-    return Model(
-        predict_real=predict_real,
-        predict_fake=predict_fake,
-        discrim_loss=discrim_loss,
-        discrim_grads_and_vars=discrim_grads_and_vars,
-        gen_loss_GAN=gen_loss_GAN,
-        gen_loss_L1=gen_loss_L1,
-        gen_grads_and_vars=gen_grads_and_vars,
-        outputs=outputs,
-        train=tf.group(incr_global_step, gen_train),
-        gradient_penalty=gradient_penalty,
-    )
-
-def preprocess(image):
-    with tf.name_scope("preprocess"):
-        # [0, 1] => [-1, 1]
-        return image * 2 - 1
-
-def deprocess(image):
-    with tf.name_scope("deprocess"):
-        # [-1, 1] => [0, 1]
-        return (image + 1) / 2
-
-def discrim_conv(args, batch_input, out_channels, stride):
+def discrim_conv(batch_input, out_channels, stride, init_stddev):
     padded_input = tf.pad(batch_input, [[0, 0], [1, 1], [1, 1], [0, 0]], mode="CONSTANT")
-    return tf.layers.conv2d(padded_input, out_channels, kernel_size=4, strides=(stride, stride), padding="valid", kernel_initializer=tf.random_normal_initializer(0, args["init_stddev"]))
+    return tf.layers.conv2d(padded_input, out_channels, kernel_size=4, strides=(stride, stride), padding="valid", kernel_initializer=tf.random_normal_initializer(0, init_stddev))
 
-
-def gen_conv(args, batch_input, out_channels):
+def gen_conv(batch_input, out_channels, init_stddev, separable_conv):
     # [batch, in_height, in_width, in_channels] => [batch, out_height, out_width, out_channels]
-    initializer = tf.random_normal_initializer(0, args["init_stddev"])
-    if args["separable_conv"]:
+    initializer = tf.random_normal_initializer(0, init_stddev)
+    if separable_conv:
         return tf.layers.separable_conv2d(batch_input, out_channels, kernel_size=4, strides=(2, 2), padding="same", depthwise_initializer=initializer, pointwise_initializer=initializer)
     else:
         return tf.layers.conv2d(batch_input, out_channels, kernel_size=4, strides=(2, 2), padding="same", kernel_initializer=initializer)
 
-
-def gen_deconv(args, batch_input, out_channels):
+def gen_deconv(batch_input, out_channels, init_stddev, separable_conv):
     # [batch, in_height, in_width, in_channels] => [batch, out_height, out_width, out_channels]
-    initializer = tf.random_normal_initializer(0, args["init_stddev"])
-    if args["separable_conv"]:
+    initializer = tf.random_normal_initializer(0, init_stddev)
+    if separable_conv:
         _b, h, w, _c = batch_input.shape
         resized_input = tf.image.resize_images(batch_input, [h * 2, w * 2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         return tf.layers.separable_conv2d(resized_input, out_channels, kernel_size=4, strides=(1, 1), padding="same", depthwise_initializer=initializer, pointwise_initializer=initializer)
     else:
         return tf.layers.conv2d_transpose(batch_input, out_channels, kernel_size=4, strides=(2, 2), padding="same", kernel_initializer=initializer)
 
-def lrelu(x, a):
-    return tf.nn.leaky_relu(x, a)
-
-def batchnorm(args, inputs):
-    return tf.layers.batch_normalization(inputs, axis=3, epsilon=1e-5, momentum=0.1, training=True, gamma_initializer=tf.random_normal_initializer(1.0, args["init_stddev"]))
+def batchnorm(inputs, init_stddev):
+    return tf.layers.batch_normalization(inputs, axis=3, epsilon=1e-5, momentum=0.1, training=True, gamma_initializer=tf.random_normal_initializer(1.0, init_stddev))
 
 def layernorm(inputs):
     return tf.contrib.layers.layer_norm(inputs)
@@ -162,7 +151,7 @@ def create_generator(args, generator_inputs, generator_outputs_channels):
 
     # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
     with tf.variable_scope("encoder_1"):
-        output = gen_conv(args, generator_inputs, args["ngf"])
+        output = gen_conv(generator_inputs, args["ngf"], args["init_stddev"], args["separable_conv"])
         layers.append(output)
 
     layer_specs = [
@@ -177,11 +166,11 @@ def create_generator(args, generator_inputs, generator_outputs_channels):
 
     for out_channels in layer_specs:
         with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
-            rectified = lrelu(layers[-1], 0.2)
+            rectified = tf.nn.leaky_relu(layers[-1], 0.2)
             # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels]
-            convolved = gen_conv(args, rectified, out_channels)
+            convolved = gen_conv(rectified, out_channels, args["init_stddev"], args["separable_conv"])
             if not args["no_gen_bn"]:
-                convolved = layernorm(convolved) if args["layer_norm"] else batchnorm(args, convolved)
+                convolved = layernorm(convolved) if args["layer_norm"] else batchnorm(convolved, args["init_stddev"])
             layers.append(convolved)
 
     layer_specs = [
@@ -207,9 +196,9 @@ def create_generator(args, generator_inputs, generator_outputs_channels):
 
             rectified = tf.nn.relu(input)
             # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
-            output = gen_deconv(args, rectified, out_channels)
+            output = gen_deconv(rectified, out_channels, args["init_stddev"], args["separable_conv"])
             if not args["no_gen_bn"]:
-                output = layernorm(output) if args["layer_norm"] else batchnorm(args, output)
+                output = layernorm(output) if args["layer_norm"] else batchnorm(output, args["init_stddev"])
 
             if dropout > 0.0:
                 output = tf.nn.dropout(output, keep_prob=1 - dropout)
@@ -222,8 +211,8 @@ def create_generator(args, generator_inputs, generator_outputs_channels):
         rectified = tf.nn.relu(input)
         if args["angle_output"]:
             # Produce 3D unit vector from 2 angles
-            angle_x = gen_deconv(args, rectified, 1)
-            angle_y = gen_deconv(args, rectified, 1)
+            angle_x = gen_deconv(rectified, 1, args["init_stddev"], args["separable_conv"])
+            angle_y = gen_deconv(rectified, 1, args["init_stddev"], args["separable_conv"])
 
             sin_x, cos_x = tf.sin(angle_x), tf.cos(angle_x)
             sin_y, cos_y = tf.sin(angle_y), tf.cos(angle_y)
@@ -233,7 +222,7 @@ def create_generator(args, generator_inputs, generator_outputs_channels):
 
             output = tf.concat((output_x, output_y, output_z), axis=-1, name="output")
         else:
-            output = gen_deconv(args, rectified, generator_outputs_channels)
+            output = gen_deconv(rectified, generator_outputs_channels, args["init_stddev"], args["separable_conv"])
             output = tf.tanh(output, name="output")
         layers.append(output)
 
@@ -248,8 +237,8 @@ def create_discriminator(args, discrim_inputs, discrim_targets):
 
     # layer_1: [batch, 256, 256, in_channels * 2] => [batch, 128, 128, ndf]
     with tf.variable_scope("layer_1"):
-        convolved = discrim_conv(args, input, args["ndf"], stride=2)
-        rectified = lrelu(convolved, 0.2)
+        convolved = discrim_conv(input, args["ndf"], 2, args["init_stddev"])
+        rectified = tf.nn.leaky_relu(convolved, 0.2)
         layers.append(rectified)
 
     # layer_2: [batch, 128, 128, ndf] => [batch, 64, 64, ndf * 2]
@@ -259,15 +248,15 @@ def create_discriminator(args, discrim_inputs, discrim_targets):
         with tf.variable_scope("layer_%d" % (len(layers) + 1)):
             out_channels = args["ndf"] * min(2**(i+1), 8)
             stride = 1 if i == n_layers - 1 else 2  # last layer here has stride 1
-            convolved = discrim_conv(args, layers[-1], out_channels, stride=stride)
+            convolved = discrim_conv(layers[-1], out_channels, stride, args["init_stddev"])
             if not args["no_disc_bn"]:
-                convolved = layernorm(convolved) if args["layer_norm"] else batchnorm(args, convolved)
-            rectified = lrelu(convolved, 0.2)
+                convolved = layernorm(convolved) if args["layer_norm"] else batchnorm(convolved, args["init_stddev"])
+            rectified = tf.nn.leaky_relu(convolved, 0.2)
             layers.append(rectified)
 
     # layer_5: [batch, 31, 31, ndf * 8] => [batch, 30, 30, 1]
     with tf.variable_scope("layer_%d" % (len(layers) + 1)):
-        convolved = discrim_conv(args, rectified, out_channels=1, stride=1)
+        convolved = discrim_conv(rectified, 1, 1, args["init_stddev"])
 
         # Only use sigmoid for normal GAN.
         # WGAN requires a full linear output.
